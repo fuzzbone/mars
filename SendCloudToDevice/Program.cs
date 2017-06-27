7db4f59b688601e15a521c0260ca200b424f6f04
Daniel Sniderman <fuzzbone2@gmail.com>
2017-06-27 10:39:36 -0500

    initial commit from msdn demos

---
+++ SendCloudToDevice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace SendCloudToDevice
+{
+    class Program
+    {
+        static string connectionString = "HostName=DannyS.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=63kQ8I2TzRau8ou6uRjK5Cp3oel1acGCPxcniJWtwIA=";
+        static ServiceClient serviceClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+            SendCloudToDeviceMessageAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
+            ReceiveFeedbackAsync();
+            await serviceClient.SendAsync("myFirstDevice", commandMessage);
+        }
+
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}", string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
+        }
+    }
+}
